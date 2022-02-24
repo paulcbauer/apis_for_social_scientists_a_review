a48879424e0392718354a5698bd44dd86595d0a7
chainsawriot <chainsawtiney@gmail.com>
2022-02-24 16:57:10 +0100

    Rename all chapters with the prefix `Chapter`, ref #31
    Rename using emacs's Wdired mode

--- figures/sort_chapters.R
+++ figures/sort_chapters.R
@@ -1,8 +1,9 @@
 yaml_content <- read_yaml(here::here("_bookdown.yml"))
 all_rmds <- fs::path_file(fs::dir_ls(here::here(), regexp = "^.+\\.[rR]md$"))
 index_index <- which(all_rmds == "index.Rmd")
-intro_index <- which(grepl("introduction", all_rmds))
+intro_index <- which(grepl("Introduction", all_rmds))
 bp_index <- which(grepl("Best_practices", all_rmds))
-rmd_files <- all_rmds[c(index_index, intro_index, bp_index, setdiff(seq_along(all_rmds), c(index_index, intro_index, bp_index)))]
+ref_index <- which(grepl("References_Appendix", all_rmds))
+rmd_files <- all_rmds[c(index_index, intro_index, bp_index, setdiff(seq_along(all_rmds), c(index_index, intro_index, bp_index, ref_index)), ref_index)]
 yaml_content$rmd_files <- rmd_files
 write_yaml(yaml_content, here::here("_bookdown.yml"))
